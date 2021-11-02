420e5b203831be56edf382e6ab42aad93531da76
Sara Maxwell <saraann.maxwell@gmail.com>
2021-11-01 21:04:57 -0600

    Added first two files
    Added table column names and dump file of 2020 presidential results

---
+++ california_counties_president.sql
@@ -0,0 +1,5 @@
+CREATE TABLE california(
+Year_County varchar(25), Democratic int, Republican int, Libertarian int, Reform int, Natural_Law int, Independent_1 int, Independent_2 int,
+Independent_3 int, Green int, American_Independent int, Peace_and_Freedom int, Progressive int, Independent_Progressive int, Socialist int,
+Prohibition int, National_Democrat int, Socialist_Labor int, Communist int, National int, Populist int, Independence_League int, Liberty int,
+Write_In_1 int, Write_In_2 int, Write_In_3 int, Write_In_4 int, Write_In_5 int, Write_In_6 int, Write_In_7 int, Write_In_8 int, PRIMARY KEY(`Year_County`));
