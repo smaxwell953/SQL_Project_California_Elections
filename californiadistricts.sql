0e28287f09e4dc1d4621373329a25714105f55b1
Sara Maxwell <saraann.maxwell@gmail.com>
2021-11-12 19:01:42 -0700

    Add files via upload

---
+++ californiadistricts.sql
@@ -0,0 +1,9 @@
+CREATE TABLE californiadistricts(
+Year_County varchar(50), Year int, County varchar(25), CA01 varchar(4), CA02 varchar(4), CA03 varchar(4), CA04 varchar(4), CA05 varchar(4),
+CA06 varchar(4), CA07 varchar(4), CA08 varchar(4), CA09 varchar(4), CA10 varchar(4), CA11 varchar(4), CA12 varchar(4), CA13 varchar(4),
+CA14 varchar(4), CA15 varchar(4), CA16 varchar(4), CA17 varchar(4), CA18 varchar(4), CA19 varchar(4), CA20 varchar(4), CA21 varchar(4),
+CA22 varchar(4), CA23 varchar(4), CA24 varchar(4), CA25 varchar(4), CA26 varchar(4), CA27 varchar(4), CA28 varchar(4), CA29 varchar(4),
+CA30 varchar(4), CA31 varchar(4), CA32 varchar(4), CA33 varchar(4), CA34 varchar(4), CA35 varchar(4), CA36 varchar(4), CA37 varchar(4),
+CA38 varchar(4), CA39 varchar(4), CA40 varchar(4), CA41 varchar(4), CA42 varchar(4), CA43 varchar(4), CA44 varchar(4), CA45 varchar(4),
+CA46 varchar(4), CA47 varchar(4), CA48 varchar(4), CA49 varchar(4), CA50 varchar(4), CA51 varchar(4), CA52 varchar(4), CA53 varchar(4),
+PRIMARY KEY(`Year_County`));
